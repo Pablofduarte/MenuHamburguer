89e81c577ad25fe67dd67163e1926d9e8b157a62
João Pablo Duarte <pablosuporte.pb@gmail.com>
2025-08-06 18:21:18 -0400

    menuResponivo

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-function menuOpen() {
-    let menuHamburguer = document.querySelector('.menuMobile');
-    if (menuHamburguer.classList.contains('open')) {
-        menuHamburguer.classList.remove('open');
-     
-    } else {
-        menuHamburguer.classList.add('open')
-    }
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
 }
 
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
